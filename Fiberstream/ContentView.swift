467ac7bec1efebbd40db0300055d35bcd56cda91
gmediaiosdev2 <maulana.husni@gmedia.co.id>
2020-04-21 10:14:38 +0700

    Initial Commit

---
+++ Fiberstream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fiberstream
+//
+//  Created by gmedia ios dev 2 on 21/04/20.
+//  Copyright © 2020 Gmedia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
